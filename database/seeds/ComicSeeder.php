049b954cd6391661175a5d3b95b8578a88606b04
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-05-17 16:31:57 +0200

    create value database

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -11,6 +12,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $Comics = config('comic');
+
+        foreach ($Comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic["title"];
+            $newComic->description = $comic["description"];
+            $newComic->thumb = $comic["thumb"];
+            $newComic->price = floatval($comic["price"]);
+            $newComic->series = $comic["series"];
+            $newComic->sale_date = DateTime::createFromFormat("Y-m-d", $comic["sale_date"]);
+            $newComic->type = $comic["type"];
+            $newComic->save();
+        }
     }
 }
